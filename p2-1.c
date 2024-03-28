8388b35a15177e0d1a3821bd38039700284789b8
a1s2d2f36g6g6q1 <vrc.maya@chungbuk.ac.kr>
2024-03-28 15:31:48 +0900

    m

--- p2-1.c
+++ p2-1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 #define MAX_SIZE 100
-
-float sum1(float list[], int);
-float sum2(float *list, int);
-float sum3(int n, float *list);
+// 최대크기 상수 정의
+float sum1(float list[], int); // 배열 자체를 받아 합을 계산
+float sum2(float *list, int);  // 배열 포인터를 받아 합을 계산
+float sum3(int n, float *list); // 배열 크기, 배열 포인터를 받아 합을 계산
 
 float input[MAX_SIZE], answer;
 int i;
@@ -12,6 +12,7 @@ int i;
 void main(void){
     for (i = 0; i < MAX_SIZE; i++)
         input[i] = i;
+        // 배열에 0~99까지 값을 초기화
     
     printf("-----------------------------------------\n");
     printf("----- [Suh Dong-Woo] [2021041094] ----- \n");
@@ -22,9 +23,10 @@ void main(void){
     printf(" sum1(input, MAX_SIZE) \n");
     printf("--------------------------------------\n");
     printf("input \t= %p\n", input);
-    
+
     answer = sum1(input, MAX_SIZE);
     printf("The sum is: %f\n\n", answer);
+    // sum1 계산 후 출력
     
     printf("--------------------------------------\n");
     printf(" sum2(input, MAX_SIZE) \n");
@@ -33,6 +35,7 @@ void main(void){
     
     answer = sum2(input, MAX_SIZE);
     printf("The sum is: %f\n\n", answer);
+    // sum2 계산 후 출력
     
     printf("--------------------------------------\n");
     printf(" sum3(MAX_SIZE, input) \n");
@@ -41,6 +44,7 @@ void main(void){
     
     answer = sum3(MAX_SIZE, input);
     printf("The sum is: %f\n\n", answer);
+    // sum3 계산 후 출력
 }
 
 float sum1(float list[], int n) {
--- p2-2.c
+++ p2-2.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 void print_one(int *ptr, int rows);
+// 포인터를 받아 배열을 출력하는 함수
 
 int main() {
     int one[] = {0, 1, 2, 3, 4};
+    // 배열 선언
 
     printf("-----------------------------------------\n");
     printf("----- [Suh Dong-Woo] [2021041094] ----- \n");
@@ -13,14 +15,15 @@ int main() {
     printf("&one = %p\n", &one);
     printf("&one[0] = %p\n", &one[0]);
     printf("\n");
+    // 배열의 주소, 자체 주소, 첫번째 원소와 주소
 
     printf("------------------------------------\n");
-    printf(" print_one(&one[0], 5) \n");
+    printf(" print_one(&one[0], 5) \n"); // 배열 첫 원소 주소와 크기 5 전달
     printf("------------------------------------\n");
     print_one(&one[0], 5);
 
     printf("------------------------------------\n");
-    printf(" print_one(one, 5) \n");
+    printf(" print_one(one, 5) \n"); // 배열 이름과 크기 5 전달
     printf("------------------------------------\n");
     print_one(one, 5);
 
