74a76ffeec666027e0873f667490585e8bc8efd1
a1s2d2f36g6g6q1 <vrc.maya@chungbuk.ac.kr>
2024-03-28 14:43:47 +0900

    asdf

--- ap1.c
+++ ap1.c
@@ -4,13 +4,16 @@
 void main(){
     int list[5];   
     int *plist[5] = {NULL,};
+	// 일반 정수 리스트와 포인터 리스트를 만듬
 
     plist[0] = (int *)malloc(sizeof(int));
+    //plist[0] 에 메모리 할당
 
     list[0] = 1;
     list[1] = 100;
 
     *plist[0] = 200;
+    // plist[0] 이 가리키는 메모리에 값 저장
 
     printf("-----------------------------------------\n");
     printf("----- [Suh Dong-Woo] [2021041094] ----- \n");
@@ -20,12 +23,14 @@ void main(){
     printf("&list[0] = %p\n", &list[0]);
     printf("list = %p\n", list);
     printf("&list = %p\n", &list);
+    // list[0] 의 값과 주소, list 배열 전체의 주소 출력 (list[0]과 동일)
 
     printf("----------------------------------------\n\n");
     printf("list[1] = %d\n", list[1]);
     printf("&list[1] = %p\n", &list[1]);
     printf("*(list+1) = %d\n", *(list + 1));
     printf("list+1 = %p\n", list+1);
+    //list[1]의 값 주소, 포인터연산을 통한 간접접근, 주소
 
     printf("----------------------------------------\n\n");
     printf("*plist[0] = %d\n", *plist[0]);
@@ -37,6 +42,8 @@ void main(){
     printf("plist[2] = %p\n", plist[2]);
     printf("plist[3] = %p\n", plist[3]);
     printf("plist[4] = %p\n", plist[4]);
-    
+    // plist[0]이 가리키는 메모리 값과 주소 (plist 배열 자체 주소 X), 각 요소 주소 출력
+
     free(plist[0]);
+    // 메모리 해제
 }
--- ap2.c
+++ ap2.c
@@ -4,25 +4,29 @@
 void main() {
     int list[5];
     int *plist[5];
+    // 똑같이 정수형배열, 포인터배열 선언
 
     list[0] = 10;
     list[1] = 11;
+    // 정수배열 0, 1번 자리 값 대입
 
     plist[0] = (int*)malloc(sizeof(int));
+    // plist[0] 이 가리키는 메모리에 값 저장
 
     printf("-----------------------------------------\n");
     printf("----- [Suh Dong-Woo] [2021041094] ----- \n");
     printf("-----------------------------------------\n");
 
-    printf("list[0] \t= %d\n", list[0]);
-    printf("list \t\t= %p\n", list);
-    printf("&list[0] \t= %p\n", &list[0]);
-    printf("list + 0 \t= %p\n", list+0);
-    printf("list + 1 \t= %p\n", list+1);
-    printf("list + 2 \t= %p\n", list+2);
-    printf("list + 3 \t= %p\n", list+3);
-    printf("list + 4 \t= %p\n", list+4);
-    printf("&list[4] \t= %p\n", &list[4]);
+    printf("list[0] \t= %d\n", list[0]);// 배열 첫 원소 값
+    printf("list \t\t= %p\n", list);// 배열 자체 메모리 주소
+    printf("&list[0] \t= %p\n", &list[0]);// 첫번째 원소 주소
+    printf("list + 0 \t= %p\n", list+0); // 0을 더한 주소 (list[0]=list자체주소)
+    printf("list + 1 \t= %p\n", list+1);// 1을 더한 주소 (두번째원소)
+    printf("list + 2 \t= %p\n", list+2);// 2를 더한 주소 (세번째원소)
+    printf("list + 3 \t= %p\n", list+3);// 3을 더한 주소 (네번째원소)
+    printf("list + 4 \t= %p\n", list+4);// 4를 더한 주소 (다섯번째원소)
+    printf("&list[4] \t= %p\n", &list[4]);// 마지막 원소 주소
 
     free(plist[0]);
+    // 메모리 해제
 }
